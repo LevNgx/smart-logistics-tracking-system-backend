bb0c6498e4f89792a00f84abedda40ba94e02ef8
LevNgx <venkat.nijagala@gmail.com>
2025-03-08 09:02:49 +0530

    Initial commit

---
+++ src/main/java/com/logistics/SmartLogisticsBackendApplication.java
@@ -0,0 +1,13 @@
+package com.logistics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartLogisticsBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartLogisticsBackendApplication.class, args);
+	}
+
+}
